91200757450c9cfea0eb0d4773796c1aea7b18b8
Tom Bleek <tom.bleek@proton.me>
2024-05-27 14:06:30 +0200

    Designed and implemented profile dropdown menu as far as corresponding functionality has been implemented as well. Added state to the parent (Header) component to allow the triggering of the dropdown menu. Used this state to keep the onHover background color of the profile section in the Header as long as the dropdown is active.

---
+++ client/src/components/navbar/profile-dropdown.tsx
@@ -0,0 +1,28 @@
+import { Avatar, AvatarFallback, Button } from "../ui/index"
+
+const ProfileDropdown = () => {
+
+
+    return (
+        <>
+            <div className='absolute right-0 top-full w-64 flex gap-4 flex-col justify-around py-6 px-8 bg-white border-2 border-gray-300 shadow-md rounded-md cursor-default'>
+                <div className='flex flex-col h-42 gap-4 items-center'>
+                    <Avatar>
+                        {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
+                        <AvatarFallback>KS</AvatarFallback>
+                    </Avatar>
+                    <h1 className="font-bold">Krishna Singha</h1>
+                </div>
+                <div className="flex flex-col">
+                    <Button variant='link' className='text-green-700' onClick={() => alert('Placeholder')}>Unlock Premium</Button>
+                    <Button variant='link' onClick={() => alert('Profile page still needs to be implemented.')}>View Profile</Button>
+                    <Button variant='link' onClick={() => alert('Settings page still needs to be implemented.')}>Settings</Button>
+                    <Button variant='link' onClick={() => alert('Placeholder')}>Help</Button>
+                </div>
+                <Button variant='destructive' onClick={() => alert('Auth still needs to be implemented.')}>Log Out</Button>
+            </div>
+        </>
+    );
+};
+
+export default ProfileDropdown;
