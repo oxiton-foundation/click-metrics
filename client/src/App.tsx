e696cda74b3bd216c62594d3cf34b5c342e6f48e
Himanshu Rai <himanshuraimau9@gmail.com>
2024-05-28 19:14:02 +0530

    QR CODE GENRATION FEATURE ADDED

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,15 @@
-import { Route, Routes } from "react-router-dom"
-import Template from "./components/navbar/template"
-import { Home, Links, QrCodes, LinkInBio, Analytics, Campaigns, CustomLinks, Settings } from "./components/pages/index"
-
+import { Route, Routes } from "react-router-dom";
+import Template from "./components/navbar/template";
+import { Home, Links, QrCodes, LinkInBio, Analytics, Campaigns, CustomLinks, Settings } from "./components/pages/index";
 
 function App() {
-
   return (
     <>
       <Routes>
         <Route path="/" element={<Template />} >
           <Route path="/" element={<Home />} />
           <Route path="/links" element={<Links />} />
-          <Route path="/qr" element={<QrCodes />} />
+          <Route path="/qr" element={<QrCodes url="www.google.com" />} /> 
           <Route path="/link/in/bio" element={<LinkInBio />} />
           <Route path="/analytics" element={<Analytics />} />
           <Route path="/campaigns" element={<Campaigns />} />
@@ -20,7 +18,7 @@ function App() {
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
