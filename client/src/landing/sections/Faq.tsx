5e6e593be2d23c34f60390b3042442bf3cd34d14
StephenStolk <shikharburman2@gmail.com>
2024-05-30 11:08:24 +0530

    Added FAQ section

---
+++ client/src/landing/sections/Faq.tsx
@@ -0,0 +1,16 @@
+import { AccordionUtil } from '../components/AccordionUtil'
+
+const Faq = () => {
+  return (
+    <>
+    <section className="w-full mt-[10%]">
+    <h2 className="flex justify-center scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 w-1/2 mx-auto">
+      Frequently Asked Questions
+    </h2>
+      <AccordionUtil />
+    </section>
+    </>
+  )
+}
+
+export default Faq
