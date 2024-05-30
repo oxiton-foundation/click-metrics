1ea141b9380807198ac8aad4b9e758e5bd516ed9
StephenStolk <shikharburman2@gmail.com>
2024-05-30 11:07:43 +0530

    Added sections of Landing Page

---
+++ client/src/landing/Landing.tsx
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar'
+import Hero from './sections/Hero'
+import Service from './sections/Service'
+import Testimonial from './sections/Testimonial'
+import Faq from './sections/Faq'
+
+const Landing = () => {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <Service />
+    <Testimonial />
+    <Faq />
+    </>
+  )
+}
+
+export default Landing
