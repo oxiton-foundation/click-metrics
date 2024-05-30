eb6aba01f213d8d4b2397d5004e5af9cdef12905
StephenStolk <shikharburman2@gmail.com>
2024-05-30 11:08:40 +0530

    Added Service Section

---
+++ client/src/landing/sections/Service.tsx
@@ -0,0 +1,17 @@
+import CardUtil from '../components/CardUtil'
+
+const Service = () => {
+  return (
+    <>
+    <main className="w-full bg-gray-100/50 mt-[10%] p-8">
+        <section className="w-[95%] flex mx-auto items-center justify-center py-10">
+            <div className="flex flex-wrap justify-evenly">
+              <CardUtil />
+            </div>
+        </section>
+    </main>
+    </>
+  )
+}
+
+export default Service
