d368b8b4a1e63939e27b5f58023f4356fe88e06b
lazybug19 <behera.santrupti02@gmail.com>
2024-06-02 00:33:05 +0530

    Feature: Theme Card

--- client/src/components/themes/slide-card.tsx
+++
@@ -1,8 +0,0 @@
-export const SlideCard = ({ props }) => {
-    return (
-        <div className="flex flex-wrap h-24 w-10">
-            <h2>{props.title}</h2>
-            <p>{props.content}</p>
-        </div>
-    )
-}
