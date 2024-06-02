6d8bb1dee88907be6150b815c330c6e3da6c5793
lazybug19 <behera.santrupti02@gmail.com>
2024-06-02 19:38:19 +0530

    Feature: Theme Page

---
+++ client/src/components/pages/theme.tsx
@@ -0,0 +1,9 @@
+const Theme = () => {
+    return (
+        <div className="bg-[rgb(244,246,250)] w-full h-full">
+            <h1>This is Theme page</h1>
+        </div>
+    );
+}
+
+export default Theme;
