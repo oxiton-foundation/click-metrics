6f083991e8baa305be433b1928a5e74e5eac94aa
lazybug19 <behera.santrupti02@gmail.com>
2024-06-02 16:59:16 +0530

    Update: Theme Cards

--- client/src/components/themes/template.tsx
+++ client/src/components/themes/template.tsx
@@ -3,18 +3,21 @@ import { SlideCards } from "./slides";
 const Template = () => {
     return (
         <>
-            <div className="relative flex flex-col m-3 overflow-x-auto hide-scroll-bar">
-                <div className="flex pb-10 hide-scroll-bar">
+            <div className="relative flex flex-col overflow-x-scroll hide-scroll-bar">
+                <div className="flex pb-10">
                     <div className="flex flex-nowrap">
                         {SlideCards.map((slide, id) => (
                             <div key={id} className="inline-block px-3">
                                 <div
-                                    className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md b hover:shadow-xl transition-shadow duration-300 ease-in-out"
-                                    style={{ backgroundImage: slide.backgroundImage }}
+                                    className="px-4 py-4 w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md b hover:shadow-xl transition-shadow duration-300 ease-in-out"
+                                    style={{
+                                        backgroundImage: slide.backgroundImage,
+                                        backgroundSize: 'cover',
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundPosition: 'center',
+                                    }}
                                 >
-                                    <h2>{slide.title}</h2>
-                                    <p>{slide.content}</p>
-
+                                    <h1 className="px-14 py-14 text-white font-bold text-4xl text-center">{slide.title}</h1>
                                 </div>
                             </div>
                         ))}
