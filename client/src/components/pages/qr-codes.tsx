7a9941128ff32fc1fed0e611736b6c03753d2c14
GOKULGSECE <gokul.g2022cce@sece.ac.in>
2024-06-08 23:33:26 +0530

    Updation of QR code

--- client/src/components/pages/qr-codes.tsx
+++ client/src/components/pages/qr-codes.tsx
@@ -1,31 +1,65 @@
-import QRCode from 'qrcode.react';
-import { saveAs } from 'file-saver';
+import React, { useState } from "react";
+import "./App.css";
+import {message} from "antd";
 
+const Qrcode: React.FC = () => {
+  const [img, setImg] = useState<string>("");
+  const [qrinput, setQrinput] = useState<string>("Example");
 
-const QrCodes = ({ url }: { url: string }) => {
-    const downloadQRCode = () => {
-        const canvas = document.getElementById('qrCode') as HTMLCanvasElement;
-        if (canvas) {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            canvas.toBlob((blob: any) => {
-                saveAs(blob, 'qr_code.png');
-            });
-        }
-    };
+  const generateQR = async () => {
+    try {
+      const url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(
+        qrinput
+      )}`;
+      setImg(url);
+      message.success("QR Code Generated Successfully");
+    } catch (error) {
+      console.error(error);
+      message.error("Try Again");
+    }
+  };
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <div className="p-4 bg-white rounded-lg shadow-lg">
-                <QRCode id="qrCode" value={url} size={256} className="rounded-md" />
-                <button 
-                    onClick={downloadQRCode} 
-                    className="mt-8 px-4 py-2 ml-10 mr-10  bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
-                >
-                    Download QR Code
-                </button>
-            </div>
-        </div>
-    );
+  const click = () => {
+    window.open(`${qrinput}`, "_blank");
+  };
+
+  const downloadQR = () => {
+    fetch(img)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "qrcode.png";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        message.success("Download Successfull");
+      });
+  };
+
+  return (
+    <div className="container">
+      <h2>QR CODE GENERATOR</h2>
+      <div className="qr-code-box">
+        {img && <img src={img} className="qr-code-image" onClick={click} />}
+      </div>
+      <form className="form_container">
+        <label>Enter the link for QR code</label>
+        <input
+          type="text"
+          placeholder="Enter the Link"
+          value={qrinput}
+          onChange={(e) => setQrinput(e.target.value)}
+        />
+        <button type="button" onClick={generateQR}>
+          Generate the QR
+        </button>
+        <button type="button" onClick={downloadQR}>
+          Download the QR
+        </button>
+      </form>
+    </div>
+  );
 };
 
-export default QrCodes;
+export default Qrcode;
