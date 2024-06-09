6ab55087a37cde423a80b014ff6df03b0448a5c2
Anirudh Sharma <anis42390@gmail.com>
2024-06-09 13:15:05 +0530

    fix: sidebar links and mobile responsiveness

--- client/src/components/navbar/sidebar.tsx
+++ client/src/components/navbar/sidebar.tsx
@@ -1,15 +1,76 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { Button } from "../ui";
 
+type SideLinkProps = {
+  to: string;
+  icon: string;
+  label: string;
+};
+
+const SideBarLinks = [
+  { to: "/", icon: "fa-solid fa-house", label: "Home" },
+  {
+    to: "/links",
+    icon: "fa-solid fa-arrow-up-right-from-square",
+    label: "Links",
+  },
+  { to: "/qr", icon: "fa-solid fa-qrcode", label: "QR Codes" },
+  { to: "/link/in/bio", icon: "fa-solid fa-paperclip", label: "Link-in-bio" },
+  { to: "/analytics", icon: "fa-solid fa-chart-simple", label: "Analytics" },
+  { to: "/campaigns", icon: "fa-solid fa-compass", label: "Campaigns" },
+  { to: "/custom/links", icon: "fa-solid fa-link", label: "Custom Links" },
+];
+
 const Sidebar = () => {
   const [isSidebarVisible, setIsSidebarVisible] = useState(true);
-
   const toggleSidebar = () => {
     setIsSidebarVisible(!isSidebarVisible);
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 1024) {
+        setIsSidebarVisible(false);
+      } else {
+        setIsSidebarVisible(true);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const SideLink = ({ to, icon, label }: SideLinkProps) => (
+    <NavLink
+      to={to}
+      onClick={() => {
+        window.innerWidth <= 1024 && setIsSidebarVisible(false);
+      }}
+      className={({ isActive }) =>
+        `px-4 py-3 rounded-md text-sm font-medium flex items-center gap-2 ${
+          isActive
+            ? "text-[#0c3ebb] bg-[#d7e0f9] hover:bg-[#f4f6fa] border-l-4 border-[#0c3ebb]"
+            : "text-gray-600 hover:bg-blue-50 hover:text-blue-600"
+        }`
+      }
+    >
+      <i className={icon}></i>
+      {label}
+    </NavLink>
+  );
+
   return (
-    <div className="flex fixed top-0 left-0 z-[100] md:static min-h-[100dvh]">
+    <div className="flex fixed top-0 left-0 z-[100] lg:static h-[100svh]">
+      {isSidebarVisible && (
+        <div
+          className="overlay fixed top-[-50vh] left-[-50vw] z-[0] w-[200vw] h-[200vh] block lg:hidden bg-[#00000079] cursor-pointer"
+          onClick={toggleSidebar}
+        ></div>
+      )}
+
       <div
         className={`relative ${
           isSidebarVisible ? "w-64" : "w-16"
@@ -17,9 +78,9 @@ const Sidebar = () => {
       >
         <button
           onClick={toggleSidebar}
-          className={`absolute top-4 ${
-            isSidebarVisible ? "right-[-2em]" : "right-[0rem] md:right-[-2em]"
-          } z-50 bg-white text-black px-3 py-2 mr-4 rounded-full h-auto shadow-md`}
+          className={`fixed top-4 ${
+            isSidebarVisible ? "left-[15rem]" : "left-[8px]"
+          } z-50 bg-white text-black px-3 py-2 mr-4 rounded-full h-auto shadow-md transition-width duration-300`}
         >
           {isSidebarVisible ? (
             <i className="fa-solid fa-xmark"></i>
@@ -29,7 +90,7 @@ const Sidebar = () => {
         </button>
 
         {isSidebarVisible && (
-          <div className="border-r-[1.5px] border-[#dbe0eb] min-w-[18rem] px-4 sticky top-0 left-0 h-full bg-white pt-6">
+          <div className="border-r-[1.5px] border-[#dbe0eb] min-w-[18rem] px-4 fixed top-0 left-0 h-full bg-white pt-6">
             <div className="w-full flex justify-center mb-6">
               <span className="text-2xl font-bold text-[#0c3ebb]">
                 <a href="/">
@@ -37,124 +98,36 @@ const Sidebar = () => {
                 </a>
               </span>
             </div>
-            <a href="/links" className="block w-full mt-4 mb-2">
+            <NavLink
+              to="/links"
+              className="block w-full mt-4 mb-2"
+              onClick={() => {
+                window.innerWidth <= 1024 && setIsSidebarVisible(false);
+              }}
+            >
               <Button className="w-full">Create New</Button>
-            </a>
+            </NavLink>
 
             <hr className="my-4" />
+
             <div className="flex flex-col gap-3">
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  `px-4 py-3 rounded-md text-sm font-medium flex items-center gap-2 ${
-                    isActive
-                      ? "text-[#0c3ebb] bg-[#d7e0f9] hover:bg-[#f4f6fa] border-l-4 border-[#0c3ebb]"
-                      : "text-gray-600 hover:bg-blue-50 hover:text-blue-600"
-                  }`
-                }
-              >
-                <i className="fa-solid fa-house"></i>
-                Home
-              </NavLink>
-              <NavLink
-                to="/links"
-                className={({ isActive }) =>
-                  `px-4 py-3 rounded-md text-sm font-medium flex items-center gap-2 ${
-                    isActive
-                      ? "text-[#0c3ebb] bg-[#d7e0f9] hover:bg-[#f4f6fa] border-l-4 border-[#0c3ebb]"
-                      : "text-gray-600 hover:bg-blue-50 hover:text-blue-600"
-                  }`
-                }
-              >
-                <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                Links
-              </NavLink>
-              <NavLink
-                to="/qr"
-                className={({ isActive }) =>
-                  `px-4 py-3 rounded-md text-sm font-medium flex items-center gap-2 ${
-                    isActive
-                      ? "text-[#0c3ebb] bg-[#d7e0f9] hover:bg-[#f4f6fa] border-l-4 border-[#0c3ebb]"
-                      : "text-gray-600 hover:bg-blue-50 hover:text-blue-600"
-                  }`
-                }
-              >
-                <i className="fa-solid fa-qrcode"></i>
-                QR Codes
-              </NavLink>
-              <NavLink
-                to="/link/in/bio"
-                className={({ isActive }) =>
-                  `px-4 py-3 rounded-md text-sm font-medium flex items-center gap-2 ${
-                    isActive
-                      ? "text-[#0c3ebb] bg-[#d7e0f9] hover:bg-[#f4f6fa] border-l-4 border-[#0c3ebb]"
-                      : "text-gray-600 hover:bg-blue-50 hover:text-blue-600"
-                  }`
-                }
-              >
-                <i className="fa-solid fa-paperclip"></i>
-                Link-in-bio
-              </NavLink>
-              <NavLink
-                to="/analytics"
-                className={({ isActive }) =>
-                  `px-4 py-3 rounded-md text-sm font-medium flex items-center gap-2 ${
-                    isActive
-                      ? "text-[#0c3ebb] bg-[#d7e0f9] hover:bg-[#f4f6fa] border-l-4 border-[#0c3ebb]"
-                      : "text-gray-600 hover:bg-blue-50 hover:text-blue-600"
-                  }`
-                }
-              >
-                <i className="fa-solid fa-chart-simple"></i>
-                Analytics
-              </NavLink>
-              <NavLink
-                to="/campaigns"
-                className={({ isActive }) =>
-                  `px-4 py-3 rounded-md text-sm font-medium flex items-center gap-2 ${
-                    isActive
-                      ? "text-[#0c3ebb] bg-[#d7e0f9] hover:bg-[#f4f6fa] border-l-4 border-[#0c3ebb]"
-                      : "text-gray-600 hover:bg-blue-50 hover:text-blue-600"
-                  }`
-                }
-              >
-                <i className="fa-solid fa-compass"></i>
-                Campaigns
-              </NavLink>
-              <NavLink
-                to="/custom/links"
-                className={({ isActive }) =>
-                  `px-4 py-3 rounded-md text-sm font-medium flex items-center gap-2 ${
-                    isActive
-                      ? "text-[#0c3ebb] bg-[#d7e0f9] hover:bg-[#f4f6fa] border-l-4 border-[#0c3ebb]"
-                      : "text-gray-600 hover:bg-blue-50 hover:text-blue-600"
-                  }`
-                }
-              >
-                <i className="fa-solid fa-link"></i>
-                Custom Links
-              </NavLink>
+              {SideBarLinks.map((link, index) => (
+                <SideLink key={index} {...link} />
+              ))}
               <hr />
-              <NavLink
+              <SideLink
                 to="/settings"
-                className={({ isActive }) =>
-                  `px-4 py-3 rounded-md text-sm font-medium flex items-center gap-2 ${
-                    isActive
-                      ? "text-[#0c3ebb] bg-[#d7e0f9] hover:bg-[#f4f6fa] border-l-4 border-[#0c3ebb]"
-                      : "text-gray-600 hover:bg-blue-50 hover:text-blue-600"
-                  }`
-                }
-              >
-                <i className="fa-solid fa-gear"></i>
-                Settings
-              </NavLink>
+                icon="fa-solid fa-gear"
+                label="Settings"
+              />
             </div>
           </div>
         )}
       </div>
+
       <div
         className={`flex-1 ${
-          isSidebarVisible ? "ml-10" : "ml-16"
+          isSidebarVisible ? "ml-8" : "ml-0"
         } transition-margin duration-300`}
       ></div>
     </div>
