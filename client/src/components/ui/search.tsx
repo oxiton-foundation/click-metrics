05133f74378a1f2c740ef721792a944b7240b54a
Utsavladia <utsavladia@gmail.com>
2024-06-13 13:16:57 +0530

    removed pachage-lock from client and reduced size of searchabar to give margin at right

--- client/src/components/ui/search.tsx
+++ client/src/components/ui/search.tsx
@@ -31,7 +31,7 @@ const Search = () => {
   return (
     <div
       className={`border-[1.5px] border-[#2a5bd7] rounded-md bg-[#f4f6fa] py-2 md:px-4 pl-2 flex gap-2 items-center w-[24rem] 
-      ${isExpanded && isSmallScreen ? "z-40" : ""} relative`}
+      ${isExpanded && isSmallScreen ? "z-40 " : ""} relative`}
     >
       <i
         className="fa-solid fa-magnifying-glass text-[#71809f] cursor-pointer"
@@ -42,7 +42,7 @@ const Search = () => {
         className={`bg-[#f4f6fa] outline-none rounded-md w-full transition-all duration-200 
         ${
           isExpanded && isSmallScreen
-            ? "absolute top--1 left-8 w-52 h-[2.5rem] z-50 px-4 border-[#2a5bd7] border-2"
+            ? "absolute top--1 left-8 w-52 h-[2.5rem] z-50  px-4 border-[#2a5bd7] border-2"
             : "relative"
         }`}
         placeholder="Search"
